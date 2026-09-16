e6847baeae4c4c371a6e878785cedb3b9981936e
Adithya2k5 <adithyagowda8147@gmail.com>
2026-09-16 12:34:58 +0530

    resolved cross-site cookies problem

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -51,7 +51,7 @@ async function registerUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, { httpOnly: true, secure: true, sameSite: "none", maxAge: 24 * 60 * 60 * 1000 });
 
     res.status(201).json({
         message: "User registered successfully",
@@ -90,8 +90,8 @@ const loginUserController = async (req, res) => {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
-    res.status(200).json({
+        res.cookie("token", token, { httpOnly: true, secure: true, sameSite: "none", maxAge: 24 * 60 * 60 * 1000 });    
+        res.status(200).json({
         messsage: "User Logged-In Successfully",
         user: {
             id: user._id,
